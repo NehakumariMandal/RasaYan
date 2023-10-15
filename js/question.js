eaa2f98aade8c661a73432ae3e9b3579a73533c7
NehakumariMandal <nehakumari120mandal@gmail.com>
2023-10-15 15:22:27 +0530

    added successfully !

---
+++ js/question.js
@@ -0,0 +1,71 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "What is the chemical formula for lithium peroxide?",
+      answer: "Li2O2",
+      options: [
+        "Li2O2",
+        "LiO2",
+        "LiO",
+        "LiO4"
+      ]
+    },
+      {
+      numb: 2,
+      question: "Mass of an atom is equals to which of the following?",
+      answer: "Neutrons and protons.",
+      options: [
+        "Protons",
+        "Neutrons and protons.",
+        "Protons and electrons",
+        "Only protons"
+      ]
+    },
+      {
+      numb: 3,
+      question: "What is the increasing order for the values of e/m for",
+      answer: "n, α, p, e",
+      options: [
+        " e, p, n, α",
+        "n, p, e, α",
+        " n, p, α, e",
+        "n, α, p, e"
+      ]
+    },
+      {
+      numb: 4,
+      question: "Nickel has atomic number 28. The correct electronic configuration is?",
+      answer: "1s2 2s2 2p6 3s2 3p6 3d8 4s2",
+      options: [
+        "1s2 2s2 2p4 3s2 3p8 3d10",
+        "1s2 2s2 2p6 3s2 3p6 3d8 4s2",
+        "1s2 2s2 2p4 3s2 3p6 4s2",
+        "1s2 2s2 3s2 3p8 3d10"
+      ]
+    },
+      {
+      numb: 5,
+      question: " What would be the atomic number of that element having full K and L shell",
+      answer: "10",
+      options: [
+        "20",
+        "10",
+        "16",
+        "14"
+      ]
+    },
+    
+      {
+       numb: 6,
+       question: "Atoms having the same mass number and different atomic number are known as",
+      answer: "Isotopes.",
+      options: [
+        "Isotopes",
+         "Isotones",
+        "Isobars",
+        "Isomers"
+       ]
+    },
+  ];
+  
